6feba53060ae3825bd26c56d82844ac160e9d9f1
mattoehler <mjoehler94@gmail.com>
2017-02-10 16:09:52 -0700

    Sync

--- MattScripts/FDR_Matt.py
+++ MattScripts/FDR_Matt.py
@@ -270,27 +270,25 @@ if __name__ == '__main__':
     data = reader.readAllCyclesForZone(run, zone)
     df = pd.DataFrame(data)
     df.columns = reader.getMetricNames()
-    # print df.head(10)
+    print df.head(5)
     
-    print "CheckPoint: 1"
     
     x = range(0,df.shape[0])
     y = df["oddy"]
     print y.head(10)
-    
-    print "CheckPoint: 2"
-    
+        
     plt.plot(x,y,'-o',color = 'blue')
     plt.xlabel("Time")
     plt.ylabel("Oddy")
     plt.show()
     
-    print "CheckPoint: 3"
+    # data = pd.read_csv(fileName)
+    # plt.figure()
+    # ax = data.plot(x='Time',y=variable, label=variable, color=c)
+    # fig =  ax.get_figure()  
+    # fig.savefig(variable+'.png')    
     
-    
-    
-
-    
+        
     #All code below is from Ming's original script
     
 #     data = reader.getMetricNames()
